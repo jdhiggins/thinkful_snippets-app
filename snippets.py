06de017a45301ed75d5aaead23ae421f16629052
jdhggins <jshhggns@gmail.com>
2015-01-29 22:15:35 -0500

    Added work from 2.1.2 to Making the Interface Accept Arguments

--- snippets.py
+++ snippets.py
@@ -1,22 +1,34 @@
+import sys
 import logging
+import argparse
 '''test of git push'''
 #Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 
 def put(name, snippet):
-  """
-  Store a snippet with an associated name.
+    """
+    Store a snippet with an associated name.
   
-  Returns the name and the snippet
-  """
-  logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
-  return name, snippet
+    Returns the name and the snippet
+    """
+    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    return name, snippet
 
 def get(name):
-  """Retrieve the snippet with a given name.
-  If there is no such snippet, create new blank snippet with that name?
-  Or,return error, ask user for a different snippet name?
-  Returns the snippet.
-  """
-  logging.error("FIXME: Unimplemented - get({!r})".format(name))
-  return ""
+    """Retrieve the snippet with a given name.
+    If there is no such snippet, create new blank snippet with that name?
+    Or,return error, ask user for a different snippet name?
+    Returns the snippet.
+    """
+    logging.error("FIXME: Unimplemented - get({!r})".format(name))
+    return ""
+
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    arguments = parser.parse_args(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
+
